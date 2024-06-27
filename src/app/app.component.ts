1cd28194cbb123358b3e44fe872601b8be6c5596
Ahmet Uysal <ahmet-uysal@outlook.de>
2024-06-28 00:20:24 +0200

    responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { MainContentComponent } from './main-content/main-content.component';
 
@@ -12,7 +11,7 @@ import { MainContentComponent } from './main-content/main-content.component';
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, MainContentComponent]
+    imports: [CommonModule, RouterOutlet, FooterComponent, MainContentComponent]
 })
 export class AppComponent {
   title = 'portfolio';
--- src/app/main-content/above-the-fold/above-the-fold.component.ts
+++ src/app/main-content/above-the-fold/above-the-fold.component.ts
@@ -2,14 +2,15 @@ import { Component, inject } from '@angular/core';
 import { MainContentComponent } from '../main-content.component';
 import { TranslationService } from '../../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { HeaderComponent } from "../../shared/header/header.component";
 
 
 @Component({
-  selector: 'app-above-the-fold',
-  standalone: true,
-  imports: [MainContentComponent, TranslateModule],
-  templateUrl: './above-the-fold.component.html',
-  styleUrl: './above-the-fold.component.scss'
+    selector: 'app-above-the-fold',
+    standalone: true,
+    templateUrl: './above-the-fold.component.html',
+    styleUrl: './above-the-fold.component.scss',
+    imports: [MainContentComponent, TranslateModule, HeaderComponent]
 })
 export class AboveTheFoldComponent {
   translate = inject(TranslationService);
